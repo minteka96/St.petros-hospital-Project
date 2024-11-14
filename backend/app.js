2748d9b0aeba62c86f5d11b2a7b6c10879c17d6f
Eibrahim <160432177+Abuabdellahh@users.noreply.github.com>
2024-11-14 03:23:53 +0300

    Resolved merge conflicts and fixed server setup

--- backend/app.js
+++ backend/app.js
@@ -3,32 +3,25 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const path = require("path");
-const tenderRoutes = require("./Routes/tender.routes")
+const tenderRoutes = require("./Routes/tender.routes");
+const router = require("./Routes/index"); // Import the router
 
 // CORS setup
 const corsOptions = {
   origin: process.env.FRONTEND_URL,
   optionsSuccessStatus: 200,
 };
-<<<<<<< HEAD
 
 // Create Express app
-=======
-// Create a variable to hold our port number
-const port = process.env.PORT;
-// Import the router
-const router = require("./Routes/index");
-// Import the path module
-// Create the webserver
->>>>>>> 30274e53dde6f1d908267b649fe6fdd9b25d85a0
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000; // Fallback to port 3000 if process.env.PORT is not defined
 
 // Middleware
 app.use(cors(corsOptions));
 app.use(express.static("public"));
 app.use(express.json());
 app.use("/api", tenderRoutes); // Use /api as the base path
+app.use("/", router); // If you have additional routes
 
 // Start the server
 app.listen(port, () => {
